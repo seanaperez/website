6b8a432d442cc6d3daf6a31df2446573b36b2860
serbyxyz <sean@sobetechguru.com>
2024-04-09 16:32:42 -0400

    a

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+export const plugins = {
+  'postcss-import': {},
+  'tailwindcss/nesting': {},
+  tailwindcss: {},
+  autoprefixer: {},
+  'postcss-preset-env': {
+    features: { 'nesting-rules': false },
+  },
+};
